3107ecd8c1dc11716be8600e3025e56bc811bf4b
Aidan Watt <aidan@hotfootdesign.co.uk>
2023-04-03 12:01:16 +0100

    Update commands to use dispatchSync (for Laravel 10)

--- src/DefaultPageHandlerExtension.php
+++ src/DefaultPageHandlerExtension.php
@@ -29,6 +29,6 @@ class DefaultPageHandlerExtension extends PageHandlerExtension
      */
     public function make(PageInterface $page)
     {
-        $this->dispatch(new MakePage($page));
+        $this->dispatchSync(new MakePage($page));
     }
 }
